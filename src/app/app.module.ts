9f6d8340b2fa6487032134feab120cc2b1e86785
dumpl1ng <32985786+dumpl1ng@users.noreply.github.com>
2019-08-25 21:26:12 -0500

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { HeaderComponent } from './header/header.component';
+import { AnimationComponent } from './auth/animation/animation.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AuthComponent,
+    HeaderComponent,
+    AnimationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/auth/animation/animation.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, AfterViewInit, ElementRef, OnDestroy } from '@angular/core';
+import { interval, Subscriber, Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-animation',
+  templateUrl: './animation.component.html',
+  styleUrls: ['./animation.component.css']
+})
+export class AnimationComponent implements OnInit, AfterViewInit, OnDestroy{
+  private spawner;
+
+  constructor(private elemtnRef: ElementRef) { }
+
+  ngOnInit() {
+
+    this.spawner = interval(500).subscribe(
+      {
+        next() {console.log('gg');}
+      }
+    )
+  }
+
+
+  // because of the shadow DOM, can't set the background color in style.css
+  ngAfterViewInit(){
+    this.elemtnRef.nativeElement.ownerDocument.body.style.backgroundColor = "rgb(38, 41, 34)";
+  }
+
+  ngOnDestroy(){
+    this.spawner.unsubscribe();
+  }
+
+}
